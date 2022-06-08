6bc109289d85e63ce7c32d48d39db671b664b35a
ManishPairroxz <manish@pairroxz.com>
2022-06-08 14:07:42 +0530

    integrate: integrate home page :lipstick:

---
+++ src/assets/js/pagescript.js
@@ -0,0 +1,82 @@
+ jQuery(document).ready(function() {
+    jQuery("#shows").click(function(e) {
+        jQuery(".dropdown_Ulist").slideToggle();
+        e.stopPropagation();
+    });
+    jQuery(document).click(function(e) {
+        if (!jQuery(e.target).is('.dropdown_Ulist, .dropdown_Ulist *')) {
+            jQuery(".dropdown_Ulist").hide();
+        }
+    });
+
+});
+
+// jQuery('.dropdown_select').on('click', function() {
+// 	 if (jQuery(this).hasClass('clicked')) {
+// 	   $(this).removeClass('clicked');
+// 	  }
+// 	  else
+// 	  {
+// 	   $(this).addClass('clicked');
+// 	  }
+
+// });
+ jQuery(document).ready(function() {
+  // user toggle
+  $('#select_any_01').click( function(e) {
+    e.stopPropagation();
+    $('#dropdown_anyL_01').slideToggle();
+  });
+
+  $('#select_any_01').click( function(e) {
+    e.stopPropagation();  
+  });
+
+  $(this).click( function() {
+    $('#dropdown_anyL_01').hide();
+  });
+   $('body').click( function() {
+    $('#dropdown_anyL_01').hide();
+  });
+
+});
+  jQuery(document).ready(function() {
+  // user toggle
+  $('#select_any_02').click( function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#dropdown_anyL_02').slideToggle();
+  });
+
+  $('#select_any_02').click( function(e) {
+    e.stopPropagation();  
+  });
+
+  $(this).click( function() {
+    $('#dropdown_anyL_02').hide();
+  });
+    $('body').click( function() {
+    $('#dropdown_anyL_02').hide();
+  });
+
+});
+   jQuery(document).ready(function() {
+  // user toggle
+  $('#select_any_03').click( function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#dropdown_anyL_03').slideToggle();
+  });
+
+  $('#select_any_03').click( function(e) {
+    e.stopPropagation();  
+  });
+
+  $(this).click( function() {
+    $('#dropdown_anyL_03').hide();
+  }); 
+  $('body').click( function() {
+    $('#dropdown_anyL_03').hide();
+  });
+
+});
